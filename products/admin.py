1a2e218cec165fcf43bc6623f92494cde9c2eb82
Dipanshu Kakshapati <dipanshu.ksh10@gmail.com>
2023-08-27 11:38:16 +0545

    checkout

--- products/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from products.models import Product
-
-# Register your models here.
-admin.site.register(Product)
--- products/productsviews.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import redirect, render, get_object_or_404
-from products.models import Product
-
-def showdata(request):
-    results=Product.objects.all()
-    return render(request, 'products.html',{"data":results})
